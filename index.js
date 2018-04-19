e73bce37a7ecd9f696a385ff7a62b8f050984e31
Tran1560601 <kidtran457@gmail.com>
2018-04-19 15:22:49 +0700

    Hoan thanh load tren trang html

--- index.js
+++ index.js
@@ -11,26 +11,33 @@ var config = {
     max: 10, // max number of connection can be open to database
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
+
+app.get('/', (req, res) => {
+     res.sendfile(__dirname + '/dssv.html')
+})
+
 	
 var pool = new pg.Pool(config);
-app.get('/', (req, res) => {
+app.get('/load', (req, res)=>{
     pool.connect(function(err,client,done) {
         if(err){
             console.log("not able to get connection "+ err);
             res.status(400).send(err);
         } 
-        client.query('SELECT h.mahs, h.hoten, h.diachi, l.tenlop FROM hocsinh h, lop l WHERE h.malop = l.malop' ,function(err,result) {
+        client.query(`SELECT h.mahs, h.hoten, h.diachi, l.tenlop 
+                    FROM hocsinh h, lop l 
+                    WHERE h.malop = l.malop
+                    ORDER BY mahs ASC `
+        ,function(err,result) {
            //call `done()` to release the client back to the pool
             done(); 
             if(err){
                 res.end();
                 console.log(err);
-                res.status(400).send(err);
+                res.status(400).send(err)
+
             }
-            //res.status(200).send(result.rows);
-            //console.log(result.rows[0].hoten);
-            res.sendfile(__dirname + '/dssv.html', {list:result})
-            //res.render(__dirname + '/dssv.html', {ds:result})
+            res.json(result.rows)
         });
      });
 })
